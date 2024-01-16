655fa9a70e35c2e496b0e61dd24f1e9edad0893f
“iamIlyaAlex” <blagas77@me.com>
2024-01-16 17:29:03 +0300

    added less2/Task3

--- less2/Task3/Program.cs
+++ less2/Task3/Program.cs
@@ -33,17 +33,55 @@
 //     i = i + 1;
 // }
 
-int N = 10;
-int[] arr = { 2, 3, 5, 6, 8, 4, 10, 13, 18, 20 };
-int i = 0;
-int max = arr[0];
+// int N = 10;
+// int[] arr = { 2, 3, 5, 6, 8, 4, 10, 13, 18, 20 };
+// int i = 0;
+// int max = arr[0];
 
-while (i < N)
-{
-    if (arr[i] > max)
-    {
-        max = arr[i];
-    }
-    i = i + 1;
-}
-Console.Write(max);
+// while (i < N) // While
+// {
+//     if (arr[i] > max)
+//     {
+//         max = arr[i];
+//     }
+//     i = i + 1;
+// }
+// Console.WriteLine(max);
+
+// max = arr[0];
+// for (int j = 0; j < N; j++) // For
+// {
+//     if (arr[j] > max)
+//     {
+//         max = arr[j];
+//     }
+// }
+// Console.WriteLine(max);
+
+// max = arr[0];
+// foreach (int e in arr) // Foreach
+// {
+//     if (e > max)
+//     {
+//         max = e;
+//     }
+// }
+// Console.WriteLine(max);
+
+
+// Проход по элемнтам массива:
+// int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+// for (int i = 0; i < arr.Length; i++)
+// {
+//     Console.Write($"{arr[i]} ");
+// }
+
+// Задача где число итераций фиксированно:
+// int N = 100;
+// int sum = 0;
+
+// for (int i = 1; i < N; i++)
+// {
+//     sum = sum + i;
+// }
+// Console.Write(sum);
